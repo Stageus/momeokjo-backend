5b165bbf7c9b3f3a52c6b893d9951012241310bf
ctendor <109571967+ctendor@users.noreply.github.com>
2025-03-30 13:57:27 +0900

    Auth (#4) 인증 API 및 미들웨어 개발 완료하여 PR 진행
    * chore: gitignore 수정
    
    * docs: 데이터베이스 관련 파일 추가 및 폴더 이름 변경
    
    - erd 파일 추가
    - 테이블 생성 쿼리 파일 추가
    
    * feat: 신규 customWrapper 추가
    
    - 트랜잭션이 필요없는 작업에 사용하는 wrapper
    - 트랜잭션이 필요한 작업에 사용하는 wrapper
    
    * feat: express.json 미들웨어 추가
    
    * feat: 누락된 require pool 추가
    
    * feat: 기본 API 탬플릿 작성
    
    * feat: auth 기능 개발
    
    * feat: auth 코드 수정
    
    * feat: db middleware 삭제
    
    ---------
    
    Co-authored-by: blueGyu <bluegyufordev@gmail.com>
    Co-authored-by: blueGyu <165542749+blueGyu@users.noreply.github.com>

---
+++ middlewares/validator.js
@@ -0,0 +1,16 @@
+const { validationResult } = require("express-validator");
+
+exports.validate = (validations) => async (req, res, next) => {
+  await Promise.all(validations.map((validation) => validation.run(req)));
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = errors.array()[0];
+    return res.status(400).json({
+      message: "입력값 확인 필요",
+      target: error.param,
+    });
+  }
+
+  next();
+};
