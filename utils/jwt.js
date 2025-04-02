ff1d65f23d2fa05288c45e879df0a2d5f6d6f309
blueGyu <bluegyufordev@gmail.com>
2025-04-03 02:37:35 +0900

    feat: 토큰 생성 유틸 함수 작성

---
+++ utils/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+exports.createAccessToken = (payload, expiresIn) => {
+  const token = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn });
+
+  return token;
+};
+
+exports.createRefreshToken = (payload, expiresIn) => {
+  const token = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn });
+
+  return token;
+};
+
+exports.verifyToken = (token, isRefresh) => {
+  try {
+    const key = isRefresh ? process.env.JWT_ACCESS_SECRET : process.env.JWT_REFRESH_SECRET;
+
+    const decoded = jwt.verify(token, key);
+
+    return { isValid: true, results: decoded };
+  } catch (err) {
+    return { isValid: false, results: err.message };
+  }
+};
