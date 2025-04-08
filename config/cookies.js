709204c5de8054c01c707ea7bac40436d62cad1a
blueGyu <bluegyufordev@gmail.com>
2025-04-09 02:38:27 +0900

    refactor: 기본 쿠키 옵션 분리

--- config/cookies.js
+++ config/cookies.js
@@ -1,13 +1,15 @@
-exports.accessTokenOptions = {
+exports.baseCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "Strict",
+};
+
+exports.accessTokenOptions = {
+  ...exports.baseCookieOptions,
   maxAge: 60 * 15 * 1000,
 };
 
 exports.refreshTokenOptions = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "Strict",
+  ...exports.baseCookieOptions,
   maxAge: 60 * 60 * 30 * 1000,
 };
