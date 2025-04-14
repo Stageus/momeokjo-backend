cb41681882ad756babd5f729df4b2f6629681208
blueGyu <bluegyufordev@gmail.com>
2025-04-15 04:11:54 +0900

    feat: restaurants 도메인 업데이트
    - router: 미들웨어 추가
    - controller: 토큰 추가

--- domains/restaurants/router.js
+++ domains/restaurants/router.js
@@ -1,47 +1,135 @@
 const router = require("express").Router();
 const upload = require("../../config/aws-s3");
+const { createValidateChain } = require("../../middlewares/createValidateChain");
+const { validateRequest } = require("../../middlewares/validateRequest");
 const rc = require("./controller");
+const schema = require("./schema");
+const verifyAccessToken = require("../../middlewares");
 
 // 음식점 리스트 조회
-router.get("/", rc.getRestaurantInfoList);
+router.get(
+  "/",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.getRestaurantInfoList),
+  validateRequest,
+  rc.getRestaurantInfoList
+);
 
 // 음식점 등록
-router.post("/", rc.createRestaurantInfo);
+router.post(
+  "/",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.getRestaurantInfoList),
+  validateRequest,
+  rc.createRestaurantInfo
+);
 
 // 음식점 카테고리 리스트 조회
-router.get("/categories", rc.getRestaurantCategoryList);
+router.get(
+  "/categories",
+  createValidateChain(schema.getRestaurantCategoryList),
+  validateRequest,
+  rc.getRestaurantCategoryList
+);
 
 // 음식점 카테고리 등록
-router.post("/categories", rc.createRestaurantCategory);
+router.post(
+  "/categories",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.createRestaurantCategory),
+  validateRequest,
+  rc.createRestaurantCategory
+);
 
 // 음식점 카테고리 수정
-router.put("/categories/:category_idx", rc.updateRestaurantCategoryByIdx);
+router.put(
+  "/categories/:category_idx",
+  createValidateChain(schema.updateRestaurantCategoryByIdx),
+  validateRequest,
+  rc.updateRestaurantCategoryByIdx
+);
 
 // 음식점 랜덤 조회
-router.get("/recommends", rc.getRecommendRestaurant);
+router.get(
+  "/recommends",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.getRecommendRestaurant),
+  validateRequest,
+  rc.getRecommendRestaurant
+);
 
 // 메뉴 후기 리스트 조회
-router.get("/menus/:menu_idx/reviews", rc.getMenuReviewInfoList);
+router.get(
+  "/menus/:menu_idx/reviews",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.getMenuReviewInfoList),
+  validateRequest,
+  rc.getMenuReviewInfoList
+);
 
 // 메뉴 후기 등록
-router.post("/menus/:menu_idx/reviews", upload.single("image"), rc.createMenuReview);
+router.post(
+  "/menus/:menu_idx/reviews",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.createMenuReview),
+  validateRequest,
+  upload.single("image"),
+  rc.createMenuReview
+);
 
 // 메뉴 후기 수정
-router.put("/menus/reviews/:review_idx", upload.single("image"), rc.updateMenuReviewByIdx);
+router.put(
+  "/menus/reviews/:review_idx",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.updateMenuReviewByIdx),
+  validateRequest,
+  upload.single("image"),
+  rc.updateMenuReviewByIdx
+);
 
 // 음식점 메뉴 리스트 조회
-router.get("/:restaurant_idx/menus", rc.getRestaurantMenuInfoList);
+router.get(
+  "/:restaurant_idx/menus",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.getRestaurantMenuInfoList),
+  validateRequest,
+  rc.getRestaurantMenuInfoList
+);
 
 // 음식점 메뉴 등록
-router.post("/:restaurant_idx/menus", rc.createRestaurantMenu);
+router.post(
+  "/:restaurant_idx/menus",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.createRestaurantMenu),
+  validateRequest,
+  rc.createRestaurantMenu
+);
 
 // 음식점 메뉴 수정
-router.put("/menus/:menu_idx", rc.updateRestaurantMenuByIdx);
+router.put(
+  "/menus/:menu_idx",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.updateRestaurantMenuByIdx),
+  validateRequest,
+  rc.updateRestaurantMenuByIdx
+);
 
 // 음식점 상세보기 조회
-router.get("/:restaurant_idx", rc.getRestaurantInfoByIdx);
+router.get(
+  "/:restaurant_idx",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.getRestaurantInfoByIdx),
+  validateRequest,
+  rc.getRestaurantInfoByIdx
+);
 
 // 음식점 상세보기 수정
-router.put("/:restaurant_idx", rc.updateRestaurantInfoByIdx);
+router.put(
+  "/:restaurant_idx",
+  verifyAccessToken("accessToken"),
+  createValidateChain(schema.updateRestaurantInfoByIdx),
+  validateRequest,
+  rc.updateRestaurantInfoByIdx
+);
 
 module.exports = router;
