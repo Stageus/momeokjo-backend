0f416e4da0f4238ffe2f3dee390989898e6757af
blueGyu <bluegyufordev@gmail.com>
2025-04-16 04:22:36 +0900

    feat: users.lists 테이블 유니크 제약 예외처리 로직 추가

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,19 @@
 const errorHandler = (err, req, res, next) => {
-  const { status, message } = err;
+  const { status, message, code, constraint } = err;
+
+  // console.log(err);
+  if (code === "23505") {
+    if (constraint === "lists_id_key") {
+      res.status(409).json({ message: "중복 아이디 회원 있음", target: "id" });
+    } else if (constraint === "lists_nickname_key") {
+      res.status(409).json({ message: "중복 닉네임 회원 있음", target: "nickname" });
+    } else if (constraint === "lists_email_key") {
+      res.status(409).json({ message: "중복 이메일 회원 있음", target: "email" });
+    } else {
+      res.status(409).json({ message, target: constraint });
+    }
+  }
+
   res.status(status || 500).json({ message: message || "서버에 오류 발생" });
 };
 
