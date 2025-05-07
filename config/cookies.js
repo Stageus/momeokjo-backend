3c48ff7328debdfad6fda6dacd6afff8ade242f1
blueGyu <bluegyufordev@gmail.com>
2025-05-07 16:47:59 +0900

    chore: 쿠키 수명 옵션 수정

--- config/cookies.js
+++ config/cookies.js
@@ -1,15 +1,15 @@
 exports.baseCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production" || process.env.NODE_ENV === "develop",
-  sameSite: "None",
+  sameSite: process.env.NODE_ENV === "local" ? "Lax" : "None",
 };
 
 exports.accessTokenOptions = {
   ...exports.baseCookieOptions,
-  expires: new Date(Date.now() + 60 * parseInt(process.env.JWT_ACCESS_EXPIRES_IN) * 1000),
+  maxAge: 60 * 1000 * parseInt(process.env.JWT_ACCESS_EXPIRES_IN),
 };
 
 exports.refreshTokenOptions = {
   ...exports.baseCookieOptions,
-  expires: new Date(Date.now() + 60 * 60 * parseInt(process.env.JWT_REFRESH_EXPIRES_IN) * 1000),
+  maxAge: 60 * 60 * 1000 * parseInt(process.env.JWT_REFRESH_EXPIRES_IN),
 };
