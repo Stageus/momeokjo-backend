41b43bc7f9bae476cbd1984c0ef5a79db1623834
blueGyu <bluegyufordev@gmail.com>
2025-05-16 16:57:09 +0900

    feat: 관리자 로그인 api 추가

--- server.js
+++ server.js
@@ -20,12 +20,14 @@ app.use(express.json());
 const authRouter = require("./domains/auth/router");
 const restaurantsRouter = require("./domains/restaurants/router");
 const usersRouter = require("./domains/users/router");
+const adminRouter = require("./domains/admin/router");
 const errorHandler = require("./middlewares/errorHandler");
 const notFoundHandler = require("./middlewares/notFoundHandler");
 
 app.use("/auth", authRouter);
 app.use("/restaurants", restaurantsRouter);
 app.use("/users", usersRouter);
+app.use("/admin", adminRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
